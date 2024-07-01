0e6568ff17cfbcaf4b1da7c96e01755400f8a1a4
Swagatika16 <swagatikapatra1610@gmail.com>
2024-07-01 21:08:32 +0530

    reverse the numbers including negative numbers also

---
+++ Reverse_integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int temp = x ;
+        int rem = 0;
+        int  rev = 0;
+        while (temp != 0){
+            rem = temp % 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && rem > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && rem < -8)) return 0;
+            rev = rev* 10 + rem ;
+            temp = temp / 10 ;
+        }
+        return rev;
+    }
+} 
+// input = 123 
+// output = 321
+// input = -123
+// output = -321
